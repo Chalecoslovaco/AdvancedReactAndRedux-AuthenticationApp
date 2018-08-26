324dba73cd755b21fc152073008dc92ed86eb2af
Sergio García <sergiogarher@gmail.com>
2018-08-26 16:41:29 +0200

    Add logic to authentication controller

--- server/controllers/authentication.js
+++ server/controllers/authentication.js
@@ -1,3 +1,28 @@
+const User = require('../models/user');
+
 exports.signup = (req, res, next) => {
-    res.send({ success: true });
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // Check if the user's mail exists
+    User.findOne({ email }, function(err, existingUser) {
+        if(err) return next(err);
+
+        // If exists return error
+        if(existingUser) return res.status(422).send({ error: 'Email is already registered' });
+    
+        // If doesn't exists create the user
+        const user = new User({
+            email: email,
+            password: password
+        });
+
+        // Then save the user into the db
+        user.save( err => {
+            if (err) return next(err);
+
+            // Respond to the request
+            res.json(user);
+        });
+    });
 }
