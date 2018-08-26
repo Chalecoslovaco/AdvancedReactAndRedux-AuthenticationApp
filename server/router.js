7bb061459d4a3e2e0a523df9b1a97e550608ae7f
Sergio García <sergiogarher@gmail.com>
2018-08-26 18:28:26 +0200

    Create local login strategy with password + Add compare passwords logic in user model

--- server/router.js
+++ server/router.js
@@ -1,4 +1,9 @@
 const Authentication = require('./controllers/authentication');
+const passport = require('passport');
+
+const passportService = require('./services/passport');
+
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = app => {
     app.post('/signup', Authentication.signup)
--- server/services/passport.js
+++ server/services/passport.js
@@ -1,9 +1,30 @@
 const passport = require('passport');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
 
 const User = require('../models/user');
 const config = require('../config');
+
+// Local Strategy
+const localOptions = { userNameField: 'email'};
+const localLogin = new LocalStrategy(localOptions, function(email, password, done) {
+    // Verify user email and password
+    // If correct call done with the user, if not call done with false
+    User.findOne({ email: email }, function(err, user) {
+        if(err) return done(err);
+        if(!user) return done(null, false);
+
+        // Compare passwords
+        user.comparePassword(password, function(err, isMatch) {
+            if(err) return done(err);
+            if(!isMatch) return done(null, false);
+
+            return done(null, user);
+        });
+    });
+
+});
 
 // Set up options for JWT Strategy
 const jwtOptions = {
@@ -24,3 +45,4 @@ const jwtLogin = new JwtStrategy(jwtOptions, function(payload, done) {
 
 // Tell passport to use this strategy
 passport.use(jwtLogin);
+passport.use(localLogin);
