37d207eb83f368546d43aadf2d1f283b2c32b5b2
Sergio García <sergiogarher@gmail.com>
2018-08-27 17:25:36 +0200

    Create Header and Welcome Components + Add some routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header';
 
-export default () => {
+export default ({ children }) => {
     return (
         <div>
-            App component div.
+            <Header />
+            { children }
         </div>
     );
 };
---
+++ client/src/components/Welcome.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default () => {
+    return <h3>Welcome! SignUp or Sign In</h3>;
+};
