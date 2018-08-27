e66ee565222c993b0bcf7fa2f48b1253ea14efbb
Sergio García <sergiogarher@gmail.com>
2018-08-27 19:17:27 +0200

    Handle signup request success and fail
    - Create AUTH_ERROR type
    - Dispatch AUTH_USER on request success and AUTH_ERROR on request fail
    - Handle both cases in auth reducer
    - Display error message in Signup component

--- client/src/components/auth/Signup.js
+++ client/src/components/auth/Signup.js
@@ -33,13 +33,18 @@ class Signup extends Component {
                         autoComplete="none"
                     />
                 </fieldset>
+                <div>{this.props.errorMessage}</div>
                 <button>Sign Up</button>
             </form>
         );
     }
 }
 
+function mapStateToProps({ auth }) {
+    return { errorMessage: auth.errorMessage }
+}
+
 export default compose(
-    connect(null, actions),
+    connect(mapStateToProps, actions),
     reduxForm({ form: 'signup' })
 )(Signup);
